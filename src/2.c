77c97a768e3318024f0be4f05f78059eec5fba4e
l0mgwyneth <l0mgwyneth@gmx.com>
2025-03-06 09:57:20 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Coding Project</h1>
+
+<p>This is my coding project.</p>
+
+<pre><code class="c">#include &lt;stdio.h&gt;
+
+int main() {
+  printf("Hello, world!\n");
+  return 0;
+}
+</code></pre>
+
+</body>
+</html>
