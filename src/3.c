cab68c5e36044bb5acb30b186851464d498d8544
l0mgwyneth <l0mgwyneth@gmx.com>
2025-03-07 06:02:49 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = a + b;
+    printf("%d", c);
+    return 0;
+}
